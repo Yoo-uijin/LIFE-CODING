48415250ea6fabe120ee01a5662fbb661237809d
Yoo-uijin <timetam24@naver.com>
2023-04-13 08:04:46 +0900

    initial commit

---
+++ study-react/src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="REACT" />
+      <Nav />
+      <Article title="Welcome" body="Hello, WEB" />
+    </div>
+  );
+}
+
+export default App;
